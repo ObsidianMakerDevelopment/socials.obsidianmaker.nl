a751802aaa4cf00c0e844d1c6f113cfd569db290
Jonathan Boisclair <boislcjo@uqtr.ca>
2025-10-02 13:19:17 -0400

    x

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "Status: 200 No Content"
 echo "Content-Type: text/plain"
 echo 
-
+export GIT_CONFIG_GLOBAL=/dev/null 
 cd /opt/socials.obsidianmaker.nl/;
 git config --global --add safe.directory /opt/socials.obsidianmaker.nl 2>&1 >/dev/null
 git pull 2>&1 #>/dev/null
